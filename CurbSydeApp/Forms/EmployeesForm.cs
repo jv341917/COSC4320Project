3e420d28ce7e219acfe9247e0cd195cf49ecf016
jv341917 <jaisennecat@gmail.com>
2025-11-11 15:59:02 -0600

    Initial upload
     after Project Design Assignment. Contains every file in my computer project folder where VS stores it.

---
+++ CurbSydeApp/Forms/EmployeesForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurbSydeApp.Forms
+{
+    public partial class EmployeesForm : Form
+    {
+        public EmployeesForm()
+        {
+            InitializeComponent();
+        }
+        //Switches to "OverviewForm"
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            OverviewForm overviewScreen = new OverviewForm();
+            overviewScreen.Dock = DockStyle.Fill;
+            overviewScreen.TopLevel = false;
+            MainForm.mainPanel.Controls.Clear();
+            MainForm.mainPanel.Controls.Add(overviewScreen);
+            overviewScreen.Show();
+
+            this.Close();
+        }
+        //Modify each label so "Name" = employee name, "Activity" = activity, & labels disappear if an employee isn't clocked in
+        //Modify timeLabel so "00:00" changes with each time change
+    }
+}
